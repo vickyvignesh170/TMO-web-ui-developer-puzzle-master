ae277b4ad29bbc0c6bba987b0afe27df91eb53ff
vinu haran <vickyvignesh170@gmail.com>
2025-03-12 15:35:39 +0530

    Code fixes and review
    Code fixes and review

---
+++ libs/books/data-access/src/lib/+state/reading-list.reducer.ts
@@ -0,0 +1,66 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
+
+import * as ReadingListActions from './reading-list.actions';
+import { ReadingListItem } from '@tmo/shared/models';
+
+export const READING_LIST_FEATURE_KEY = 'readingList';
+
+export interface State extends EntityState<ReadingListItem> {
+  loaded: boolean;
+  error: null | string;
+}
+
+export interface ReadingListPartialState {
+  readonly [READING_LIST_FEATURE_KEY]: State;
+}
+
+export const readingListAdapter: EntityAdapter<ReadingListItem> = createEntityAdapter<
+  ReadingListItem
+>({
+  selectId: item => item.bookId
+});
+
+export const initialState: State = readingListAdapter.getInitialState({
+  loaded: false,
+  error: null
+});
+
+const readingListReducer = createReducer(
+  initialState,
+  on(ReadingListActions.init, state => {
+    return {
+      ...state,
+      loaded: false,
+      error: null
+    };
+  }),
+  on(ReadingListActions.loadReadingListSuccess, (state, action) => {
+    return readingListAdapter.setAll(action.list, {
+      ...state,
+      loaded: true
+    });
+  }),
+  on(ReadingListActions.loadReadingListError, (state, action) => {
+    return {
+      ...state,
+      error: action.error
+    };
+  }),
+  on(ReadingListActions.addToReadingList, (state, action) =>
+    readingListAdapter.addOne({ bookId: action.book.id, ...action.book }, state)
+  ),
+  on(ReadingListActions.failedAddToReadingList, (state, action) =>
+    readingListAdapter.removeOne(action.book.id, state)
+  ),
+  on(ReadingListActions.removeFromReadingList, (state, action) =>
+    readingListAdapter.removeOne(action.item.bookId, state)
+  ),
+  on(ReadingListActions.failedRemoveFromReadingList, (state, action) =>
+    readingListAdapter.addOne({ bookId: action.item.bookId, ...action.item }, state)
+  )
+);
+
+export function reducer(state: State | undefined, action: Action) {
+  return readingListReducer(state, action);
+}
---
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {
+  addToReadingList,
+  clearSearch,
+  getAllBooks,
+  ReadingListBook,
+  searchBooks,
+} from '@tmo/books/data-access';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
+import { Book } from '@tmo/shared/models';
+import { Observable } from 'rxjs';
+
+interface SearchForm {
+  term: string;
+}
+
+type SearchControl = { [key in keyof SearchForm]: AbstractControl };
+type SearchFormGroup = FormGroup & {
+  value: SearchForm;
+  controls: SearchControl;
+};
+
+@Component({
+  selector: 'tmo-book-search',
+  templateUrl: './book-search.component.html',
+  styleUrls: ['./book-search.component.scss'],
+})
+export class BookSearchComponent implements OnInit {
+  books$: Observable<ReadingListBook[]>;
+
+  searchForm = this.fb.group({
+    term: new FormControl(''),
+  } as SearchControl) as SearchFormGroup;
+
+  constructor(
+    private readonly store: Store,
+    private readonly fb: FormBuilder
+  ) {}
+
+  get searchTerm(): string {
+    return this.searchForm.value.term;
+  }
+
+  ngOnInit(): void {
+    this.books$ = this.store.select(getAllBooks);
+  }
+
+  formatDate(date: void | string) {
+    return date
+      ? new Intl.DateTimeFormat('en-US').format(new Date(date))
+      : undefined;
+  }
+
+  addBookToReadingList(book: Book) {
+    this.store.dispatch(addToReadingList({ book }));
+  }
+
+  searchExample() {
+    this.searchForm.controls.term.setValue('javascript');
+    this.searchBooks();
+  }
+
+  searchBooks() {
+    if (!this.searchForm.value.term) this.store.dispatch(clearSearch());
+    this.store.dispatch(searchBooks({ term: this.searchTerm }));
+  }
+
+  trackByFn(index: number, item: ReadingListBook) {
+    return item.id;
+  }
+}
